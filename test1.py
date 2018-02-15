b80b1b1ba0f5902627288a04b7559165b21b425f
BCOSTON <36336015+BCOSTON@users.noreply.github.com>
2018-02-15 17:56:48 +0100

    correction raw_input

--- test1.py
+++ test1.py
@@ -1,16 +1,20 @@
-#IDENTITE branche 1
+#IDENTITE 1
 
-oAge = str(input("Quel age avez vous ? "))
+oAge = raw_input("Quel age avez vous ? ")
 while not oAge.isdigit():
     print("Veuillez entrer un nombre entier")
-    oAge = str(input("Quel age avez vous ? "))
-#oAge = int(oAge)
-#if oAge >=18:
-#	print ("bravo")
-#elif oAge <18:
-#	print ("dommage")
-#	
-#oGenre = str(input ("Quel est votre genre ? H/F "))
-#if oGenre == "H":
-#	print ("Homme")
-	
+    oAge = raw_input("Quel age avez vous ? ")
+oAge = int(oAge)
+if oAge >=18:
+
+	oGenre = raw_input ("Quel est votre genre ? H/F ")
+	if oGenre == str("H"):
+		print ("Homme")
+	elif oGenre == str("F"):
+		print ("Femme")
+	elif oGenre != str("H") + oGenre != str("F"):
+		print ("erreur")
+
+elif oAge <18:
+	print ("dommage")
+
